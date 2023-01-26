72dc4c7d132c6a69b8fcb20091e5224da1a661dc
Willeduard <willeduard@gmail.com>
2023-01-26 08:12:50 -0500

    Se agregó la plantilla register.html

--- app_basic/app.py
+++ app_basic/app.py
@@ -8,6 +8,21 @@ def home():
       return render_template('index.html')
    return render_template('index.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Recolectar datos del formulario
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        # Validar datos y guardarlos en la base de datos
+        # ...
+
+        return redirect(url_for('home'))
+
+    return render_template('register.html')
+
 if __name__ == '__main__':
    #DEBUG is SET to TRUE. CHANGE FOR PROD
    app.run(port=5000,debug=True)
